cc80f9eddfb6c187d16a4c5597c5367684807648
humannocturnal <syaputrar374@gmail.com>
2025-10-24 09:39:46 +0700

    tampilkan teks nama di samping logo dengan gaya cursive

--- app/src/main/java/com/example/composablelayout2/UIPertama.kt
+++ app/src/main/java/com/example/composablelayout2/UIPertama.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -20,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -60,6 +62,17 @@ fun ActivitasPertama(modifier: Modifier){
                         .size(size = 200.dp)
                         .padding(all = 5.dp)
                 )
+                Spacer(modifier = Modifier.width(width = 30.dp))
+                Column() {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+
+                }
             }
         }
     }

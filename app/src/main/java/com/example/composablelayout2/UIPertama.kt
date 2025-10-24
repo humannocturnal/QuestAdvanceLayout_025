45b4ece0255c629011747f68349fba07efcccc39
humannocturnal <syaputrar374@gmail.com>
2025-10-24 09:34:25 +0700

    tampilkan logo menggunakan Image di dalam Row pada ActivitasPertama

--- app/src/main/java/com/example/composablelayout2/UIPertama.kt
+++ app/src/main/java/com/example/composablelayout2/UIPertama.kt
@@ -2,12 +2,15 @@ package com.example.composablelayout2
 
 import android.R.attr.fontWeight
 import android.R.attr.text
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -15,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -46,6 +50,17 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
-        ){  }
+        ){
+            Row(){
+                val gambar = painterResource( id = R.drawable.logo)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 200.dp)
+                        .padding(all = 5.dp)
+                )
+            }
+        }
     }
 }

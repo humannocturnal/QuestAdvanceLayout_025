fbfdd1987a37e3b4284b9def9bca3f2136d0c4fc
humannocturnal <syaputrar374@gmail.com>
2025-10-25 16:27:55 +0700

    tampilan ProfileScreen dengan parameter callback

---
+++ app/src/main/java/com/example/composablelayout2/UIKedua.kt
@@ -0,0 +1,13 @@
+package com.example.composablelayout2
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen( modifier: Modifier = Modifier,
+                   onPrivacy: () -> Unit = {},
+                   onHistory: () -> Unit = {},
+                   onSettings: () -> Unit = {}
+) {
+
+}

da9191b130d9ee1fe77444e9ad4674f6f31d162c
humannocturnal <syaputrar374@gmail.com>
2025-10-25 16:33:11 +0700

    Menambahkan background dengan gradasi vertikal dari warna bgTop

--- app/src/main/java/com/example/composablelayout2/UIKedua.kt
+++ app/src/main/java/com/example/composablelayout2/UIKedua.kt
@@ -1,7 +1,15 @@
 package com.example.composablelayout2
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfileScreen( modifier: Modifier = Modifier,
@@ -9,5 +17,16 @@ fun ProfileScreen( modifier: Modifier = Modifier,
                    onHistory: () -> Unit = {},
                    onSettings: () -> Unit = {}
 ) {
+    val bgTop = Color(0xFF0E1116)
+    val bgBottom = Color (0xFF1A1F26)
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(listOf(bgTop, bgBottom))
+            )
+            .padding(horizontal = 20.dp)
+    ){
+    }
 }
